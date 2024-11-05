31702172a3aecf1e4fd7132884cfefa2972c9503
Moein Arabi <moein.mo81@gmail.com>
2024-11-05 21:05:15 +0330

    HOTEL-6 Add authentication app and update settings

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'drf_spectacular'
+    'drf_spectacular',
+    'authentication',
 ]
 
 MIDDLEWARE = [
