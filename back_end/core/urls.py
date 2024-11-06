60a93eab86b0f966d168477de54e6a864cc3b2b2
Moein Arabi <moein.mo81@gmail.com>
2024-11-06 17:51:53 +0330

    HOTEL-6 Add authentication and API schema routes to URL configuration

--- back_end/core/urls.py
+++ back_end/core/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.shortcuts import render
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
+    path('auth/', include('authentication.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('', lambda request: render(request, 'home.html')),
 ]
