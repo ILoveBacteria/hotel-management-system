e2e89abb66abe87cce591b964b96ed8b33568a12
Moein Arabi <moein.mo81@gmail.com>
2024-11-28 12:09:12 +0330

    HOTEL-47 Add PUT method to change user information

--- back_end/users/serializers.py
+++ back_end/users/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from users.models import GuestProfile
@@ -17,3 +18,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('id', 'first_name', 'last_name', 'email', 'username', 'is_staff', 'is_active', 'is_superuser', 'guest_profile')
         read_only_fields = ['email', 'username', 'is_staff', 'is_active', 'is_superuser']
+        
+    def update(self, instance, validated_data):
+        guest_profile = validated_data.pop('guest_profile')
+        for attr, value in guest_profile.items():
+            setattr(instance.guest_profile, attr, value)
+        instance.guest_profile.save()
+        return super().update(instance, validated_data)
