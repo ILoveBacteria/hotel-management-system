37f66cd66b52f007257ccd20fc1d965d2272e4f1
Moein Arabi <moein.mo81@gmail.com>
2024-11-29 12:13:14 +0330

    HOTEL-47 Add custom permission class and update permission handling in user views

--- back_end/users/views.py
+++ back_end/users/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.generics import RetrieveAPIView, RetrieveUpdateAPIView
 
-from users.models import GuestProfile
 from users.serializers import UserProfileSerializer
+from users.permissions import IsOwner
 
 
 class UserProfileView(RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
     queryset = get_user_model().objects.all()
+    permission_classes = [IsAdminUser|IsOwner]
     
 
-class CurrentUserProfileView(LoginRequiredMixin, RetrieveAPIView):
+class CurrentUserProfileView(RetrieveAPIView):
     serializer_class = UserProfileSerializer
-    login_url = '/auth/login/'
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
