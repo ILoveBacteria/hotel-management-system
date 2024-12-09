26495e4a9a41058f64a5b3985ceb458ddc616faa
Moein Arabi <moein.mo81@gmail.com>
2024-12-09 23:00:22 +0330

    HOTEL-60 Add views and URLs for user reserves management

--- back_end/users/urls.py
+++ back_end/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from users.views import UserProfileView, CurrentUserProfileView
+from users.views import UserProfileView, CurrentUserProfileView, CurrentUserReservesView, UserReservesView
 
 urlpatterns = [
     path('profile/<int:pk>/', UserProfileView.as_view(), name='user-profile'),
     path('profile/', CurrentUserProfileView.as_view(), name='current-user-profile'),
+    path('reserves/', CurrentUserReservesView.as_view(), name='current-user-reserves'),
+    path('<int:pk>/reserves/', UserReservesView.as_view(), name='user-reserves'),
 ]
--- back_end/users/views.py
+++ back_end/users/views.py
@@ -1,24 +1,43 @@
+from rest_framework import generics
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.generics import RetrieveAPIView, RetrieveUpdateAPIView
 from drf_spectacular.utils import extend_schema_view
 
 from users.serializers import UserProfileSerializer
 from users.permissions import IsOwner
 from users.swagger import current_user_profile, user_profile
 
+from reservations.models import Reserve
+from reservations.serializers import ReserveSerializer
+
 
 @extend_schema_view(**user_profile)
-class UserProfileView(RetrieveUpdateAPIView):
+class UserProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
     queryset = get_user_model().objects.all()
     permission_classes = [IsAdminUser|IsOwner]
     
 
 @extend_schema_view(**current_user_profile)
-class CurrentUserProfileView(RetrieveAPIView):
+class CurrentUserProfileView(generics.RetrieveAPIView):
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
+
+
+class CurrentUserReservesView(generics.ListCreateAPIView):
+    serializer_class = ReserveSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Reserve.objects.filter(user=self.request.user)
+    
+    
+class UserReservesView(generics.ListAPIView):
+    serializer_class = ReserveSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        return Reserve.objects.filter(user_id=self.kwargs['pk'])
