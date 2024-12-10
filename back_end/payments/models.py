e71367188aac21679ef46ebe57b03b2e7ce29f6e
Moein Arabi <moein.mo81@gmail.com>
2024-12-10 18:05:37 +0330

    Fix Bill model. Add migrations

--- back_end/payments/models.py
+++ back_end/payments/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Bill(models.Model):
-    amount = models.PositiveIntegerField(max_digits=10, decimal_places=2)
+    amount = models.PositiveIntegerField()
     due_date = models.DateTimeField()
     is_paid = models.BooleanField(default=False)
-    payment_date = models.DateTimeField(null=True, blank=True)
+    payment_date = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     reserve = models.ForeignKey('reservations.Reserve', on_delete=models.CASCADE)
