e50fb1207cf8ded3aa907344de86c21d3bfc89f0
Moein Arabi <moein.mo81@gmail.com>
2024-12-10 10:06:12 +0330

    Add Bill model

--- back_end/payments/models.py
+++ back_end/payments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Bill(models.Model):
+    amount = models.PositiveIntegerField(max_digits=10, decimal_places=2)
+    due_date = models.DateTimeField()
+    is_paid = models.BooleanField(default=False)
+    payment_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reserve = models.ForeignKey('reservations.Reserve', on_delete=models.CASCADE)
