5da3ed249debd6fe6220ebb288db2bf8e4d4748b
Moein Arabi <moein.mo81@gmail.com>
2024-12-19 14:50:31 +0330

    Add not null constraint on room field for Reserve model

--- backend/reservations/models.py
+++ backend/reservations/models.py
@@ -28,7 +28,7 @@ class Reserve(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reserves')
-    room = models.ForeignKey('rooms.Room', on_delete=models.SET_NULL, related_name='reserves', null=True)
+    room = models.ForeignKey('rooms.Room', on_delete=models.PROTECT, related_name='reserves')
     
     def __str__(self):
         return f'{self.id} - {self.status}'
