f3cf02206aa8c3b25f5715cf27269a3e41665137
Moein Arabi <moein.mo81@gmail.com>
2024-12-22 10:45:45 +0330

    Add API URLs to the home.html

--- backend/rooms/urls.py
+++ backend/rooms/urls.py
@@ -5,8 +5,8 @@ from rooms.views import RoomViewSet, RoomTypeViewSet, RoomImageViewSet, RoomType
 
 
 router = SimpleRouter(use_regex_path=False)
-router.register('inventories', RoomViewSet)
-router.register('types', RoomTypeViewSet)
+router.register('inventories', RoomViewSet, basename='inventories')
+router.register('types', RoomTypeViewSet, basename='types')
 router.register('images', RoomImageViewSet, basename='images')
 # Query routers
 router.register('types/<int:room_type>/images', RoomTypeImageViewSet, basename='room-type-images')
