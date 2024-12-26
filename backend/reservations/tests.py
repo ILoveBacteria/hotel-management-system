1ba77264948691b0cd4d8e22c04459a79f11133f
Moein Arabi <moein.mo81@gmail.com>
2024-12-26 22:45:40 +0330

    Add cancel a reserve test and fix bugs in raising permission denied

--- backend/reservations/tests.py
+++ backend/reservations/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, Client
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
 
-from reservations.models import Reserve
+from reservations.models import Reserve, CancelledReserve
 from reservations import queries
 from rooms.models import RoomType
 from payments.models import Bill
@@ -136,3 +138,52 @@ class ReserveTestCase(TestCase):
         response = self.client.post(f'/rooms/types/{self.room_type.id}/reserve/', {'check_in': '2021-01-02', 'check_out': '2021-01-01'})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(Reserve.objects.count(), 0)
+        
+
+class CancelReservationTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.room_type = RoomType.objects.create(name='Test Room Type', price=100, double_beds=1, single_beds=1, description='Test Description')
+        self.room = self.room_type.rooms.create(room_number=100, is_active=True)
+        self.user1 = User.objects.create_user(username='testuser', password='testpassword')
+        self.user2 = User.objects.create_user(username='testuser2', password='testpassword2')
+        self.admin_user = User.objects.create_superuser(username='adminuser', password='adminpassword')
+        self.reserve = Reserve.objects.create(room=self.room, check_in='2021-01-01', check_out='2021-01-02', status=Reserve.REGISTERED, price=100, user=self.user2)
+    
+    def test_cancel_reserve_non_owner_permission(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('reserves-cancel', kwargs={'reserve__id': self.reserve.id})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_cancel_reserve_owner_permission(self):
+        self.client.login(username='testuser2', password='testpassword2')
+        url = reverse('reserves-cancel', kwargs={'reserve__id': self.reserve.id})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CancelledReserve.objects.count(), 1)
+        self.reserve.refresh_from_db()
+        self.assertEqual(self.reserve.status, Reserve.CANCELED)
+    
+    def test_cancel_reserve_admin_permission(self):
+        self.client.login(username='adminuser', password='adminpassword')
+        url = reverse('reserves-cancel', kwargs={'reserve__id': self.reserve.id})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CancelledReserve.objects.count(), 1)
+        self.reserve.refresh_from_db()
+        self.assertEqual(self.reserve.status, Reserve.CANCELED)
+    
+    def test_cancel_non_exist_reserve(self):
+        self.client.login(username='adminuser', password='adminpassword')
+        url = reverse('reserves-cancel', kwargs={'reserve__id': 999})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_reserve_status_change(self):
+        self.client.login(username='adminuser', password='adminpassword')
+        url = reverse('reserves-cancel', kwargs={'reserve__id': self.reserve.id})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.reserve.refresh_from_db()
+        self.assertEqual(self.reserve.status, Reserve.CANCELED)
