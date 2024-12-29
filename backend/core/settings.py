4f6cfe5d989f48996f2e17fe24893040ffe6c603
Moein Arabi <moein.mo81@gmail.com>
2024-12-29 18:49:30 +0330

    bump version from 0.5.0 to 0.5.1
    - Add Django continuous integration test workflow
    - Add Unit tests for reserve and cancel reserve APIs
    - Add tox test coverage tool for Sonarqube
    - Fix bug: permission denied was not raising for not owner reserve user in cancel API

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Bacteria Hotel API',
     'DESCRIPTION': 'Hotel management system',
-    'VERSION': '0.5.0-beta',
+    'VERSION': '0.5.1-beta',
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
