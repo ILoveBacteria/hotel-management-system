526e3f96a7ebf7a5f24e0f8112a0b152deebd09e
Moein Arabi <moein.mo81@gmail.com>
2025-01-13 21:57:46 +0330

    Fix migrations

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -149,11 +149,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'core.rest_authentication.CsrfExemptSessionAuthentication',
-    ],
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        ],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['core.rest_authentication.CsrfExemptSessionAuthentication'],
 }
 
 # Spectacular settings
