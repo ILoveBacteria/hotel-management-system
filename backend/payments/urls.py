48e93bc37ceee1c2d9c12c597ec1a8513891c865
Moein Arabi <moein.mo81@gmail.com>
2025-01-26 14:52:49 +0330

    Add IPG html template

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from payments.views import BillListView, BillDetailView, PayBillView
+from payments.views import BillListView, BillDetailView, PayBillView, PaymentGatewayView
 
 
 urlpatterns = [
     path('bills/', BillListView.as_view(), name='bill-list'),
     path('bills/<int:pk>/', BillDetailView.as_view(), name='bill-detail'),
     path('bills/<int:pk>/pay/', PayBillView.as_view(), name='pay-bill'),
+    path('pay/<int:bill_id>/', PaymentGatewayView.as_view(), name='payment-gateway'),
 ]
--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.utils import timezone
+from django.views import View
+from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
 from drf_spectacular.utils import extend_schema_view
@@ -38,3 +40,9 @@ class PayBillView(generics.GenericAPIView):
         elif bill.status == Bill.OVERDUE or timezone.now() > bill.due_date:
             return Response({'message': 'Bill is overdue.'}, status=status.HTTP_400_BAD_REQUEST)
         return redirect(f'https://ipg.moeinarabi.ir/payments/pay/{bill.id}')
+    
+
+class PaymentGatewayView(View):
+    def get(self, request, bill_id):
+        bill = get_object_or_404(Bill, id=bill_id)
+        return render(request, 'payments/payment_gateway.html', {'bill': bill})
