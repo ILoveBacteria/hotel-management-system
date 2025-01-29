c39ca6dad2294f6278893cfc135fd185e9eeb88c
Moein Arabi <moein.mo81@gmail.com>
2025-01-29 11:21:32 +0330

    Simplify room type display in admin rooms page

--- frontend/src/routes/admin/rooms/+page.svelte
+++ frontend/src/routes/admin/rooms/+page.svelte
@@ -437,9 +437,7 @@
 								<td
 									class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400"
 								>
-									{roomTypes.find(
-										(rt) => rt.id === room.room_type,
-									)?.name || 'Unknown'}
+									{room.room_type}
 								</td>
 								<td class="px-6 py-4 whitespace-nowrap">
 									<span
